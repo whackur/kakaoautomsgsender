2a8d4ecd4924eae90831d25c045169c8be3edae4
Whackur <ioumelon7@gmail.com>
2019-08-10 22:45:54 +0900

    long_msg와 logout 기능 추가

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import sys
 import pyautogui
 import time
 import pyperclip
 import os
 import random
 
+
 def send_msg(my_msg, repeat_number):
     for i in range(int(repeat_number)):
-        time_wait = random.uniform(1,3)
+        time_wait = random.uniform(3, 5)
         print('Repeat Number : ', i + 1, end='')
         print(' // Time wait : ', time_wait)
         time.sleep(time_wait)
@@ -17,15 +19,16 @@ def send_msg(my_msg, repeat_number):
         pyautogui.keyDown('esc')
         pyautogui.keyDown('down')
 
+
 def filter_friend(filter_keyword, init_number):
     # 사람 아이콘 클릭
     try:
         click_img(img_path + 'person_icon.png')
         try:
             click_img(img_path + 'person_icon2.png')
-        except Exception as e : 
+        except Exception as e :
             print('e ', e)
-    except Exception as e : 
+    except Exception as e :
         print('e ', e)
     # X 버튼이 존재한다면 클릭하여 내용 삭제
     try:
@@ -44,33 +47,71 @@ def filter_friend(filter_keyword, init_number):
         pyautogui.keyDown('down')
     time.sleep(2)
 
+
 def click_img(imagePath):
     location = pyautogui.locateCenterOnScreen(imagePath, confidence = conf)
     x, y = location
     pyautogui.click(x, y)
+
 
 def click_img_plus_x(imagePath, pixel):
     location = pyautogui.locateCenterOnScreen(imagePath, confidence = conf)
     x, y = location
     pyautogui.click(x + pixel, y)
 
+
 def doubleClickImg (imagePath):
     location = pyautogui.locateCenterOnScreen(imagePath, confidence = conf)
     x, y = location
     pyautogui.click(x, y, clicks=2)
 
+
+def set_delay():
+    delay_time = input("몇 초 후에 프로그램을 실행하시겠습니까? : ")
+    print(delay_time + "초 후에 프로그램을 실행합니다.")
+    for remaining in range(int(delay_time), 0, -1):
+        sys.stdout.write("\r")
+        sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+        sys.stdout.flush()
+        time.sleep(1)
+    sys.stdout.write("\r프로그램 실행!\n")
+
+
+def logout():
+    try:
+        click_img(img_path + 'menu.png')
+    except Exception as e:
+        print('e ', e)
+    try:
+        click_img(img_path + 'logout.png')
+    except Exception as e:
+        print('e ', e)
+
+
+def bye_msg():
+    input('프로그램이 종료되었습니다.')
+
+
+def set_import_msg():
+    with open("send_for_text.txt", "r", encoding='UTF-8') as f:
+        text = f.read()
+        print('======== 아래는 전송할 텍스트입니다. ========\n', text)
+        return text
+
+
 def initialize():
     print('Monitor size : ', end='')
     print(pyautogui.size())
     print(pyautogui.position())
-    filter_keyword = input("필터링할 친구 이름. 없으면 그냥 enter.  ex) 학생 직장 99 : ")
+    filter_keyword = input("필터링할 친구 이름. 없으면 enter.  ex) 학생 직장 99 : ")
     init_number = input("필터링한 친구 기준 시작지점(ex. 필터링된 친구 시작지점) : ")
     repeat_number = input("반복할 횟수(ex. 필터링 검색된 친구 수) : ")
-    my_msg = input("전송할 메세지 : ")
+    my_msg = input("전송할 메세지. enter를 누를 경우 send_for_text.txt를 전송 : ")
     print('=================')
     print('메세지 전송 시작!')
     print('=================')
     return (filter_keyword, init_number, repeat_number, my_msg)
+
 
 # config
 img_path = os.path.dirname(os.path.realpath(__file__)) + '/img/'
@@ -79,6 +120,18 @@ pyautogui.PAUSE = 0.5
 
 if __name__ == "__main__":
     (filter_keyword, init_number, repeat_number, my_msg) = initialize()
-    filter_friend(filter_keyword, init_number)
-    send_msg(my_msg, repeat_number)
-    # send_msg(long_msg, repeat_number)
+    print('mymsg => ', my_msg)
+    print(type(my_msg))
+    print(my_msg)
+    if len(my_msg) > 2:
+        filter_friend(filter_keyword, init_number)
+        send_msg(my_msg, repeat_number)
+        bye_msg()
+
+    else:
+        long_msg = set_import_msg()
+        set_delay()
+        filter_friend(filter_keyword, init_number)
+        send_msg(long_msg, repeat_number)
+        logout()
+        bye_msg()
